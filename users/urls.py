9b0d61e99e38c38c1b1eb44bd1288a51f697f443
YoonHwe <dbsghl676@gmail.com>
2021-07-22 22:57:00 +0900

    freeze 실행

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = "users"
 urlpatterns = [
     path('<int:id>/mypage', views.mypage, name="mypage"),
-    path('<int:id>/follow',views.follow,name="follow")
+    path('<int:id>/follow',views.follow,name="follow"),
 ]
